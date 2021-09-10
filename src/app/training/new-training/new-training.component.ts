aab33275625a43ca55bb28d9d17e1ac8a41980f0
Lelihelija <lelihelija@gmail.com>
2021-09-10 11:42:20 +0300

    3rd task done

---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { NgForm } from '@angular/forms'
+import { Observable, Subscription } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { TrainingService } from '../training.service'
+import { Exercise } from '../exercise.model'
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css'],
+})
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  exerciseSubscription: Subscription
+  exercises: Exercise[]
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    console.log('this.exerciseSubscription:', this.exerciseSubscription)
+    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(
+      (exercises) => (this.exercises = exercises),
+    )
+    this.trainingService.fetchAvailableExercises()
+  }
+
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise)
+  }
+
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe()
+  }
+}
